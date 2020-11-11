aa4e2f60d3147f9612d558aa8d7abddc2f6f0034
Stephan Stanisic <stephan@stanisic.nl>
2020-11-11 17:03:13 +0100

    a

--- .github/dangerfile.js
+++ .github/dangerfile.js
@@ -1,14 +1,28 @@
-import { message, warn, markdown, danger } from 'danger'
-var parser = require('fast-xml-parser');
+//import { message, warn, markdown, danger } from 'danger'
 const fs = require('fs');
+var parseString = require('xml2js').parseString;
 
+var xml = fs.readFileSync('.github/artifacts/result.xml');
+parseString(xml, function (err, result) {
+    result["gendarme-output"].results[0].rule.forEach(rule => {
+        let problem = rule.problem.join("\n");
+        let solution = rule.solution.join("\n");
+        let target = rule.target;
+        let name = rule["$"].Name;
+        let targetText = rule.target.map(t => t.defect[0]["$"].Location + (t.defect[0]["_"] ? (": " + t.defect[0]["_"]) : "")).join("\n");
 
-var x = fs.readFileSync('.github/artifacts/result.xml');
-var parsedXML = parser.parse(x.toString());
-console.log(parsedXML);
-/*var results = parsedXML.find(elem => elem.tagName == "results")
-console.log(results);
-results.childNodes.forEach(result => {
+        let type = message;
+        target.forEach(problem => {
+            if(problem.defect[0]["$"].Severity == "High") {
+                type = warn;
+            }
+        });
+
+        type(problem + solution + "\n" + targetText);
+    });
+});
+
+/*parsedXML["gendarme-output"].results.forEach(result => {
     console.log("result", result);
     let problem = result.childNodes.find(x => x.tagName == "problem").innerXML;
     let solution = result.childNodes.find(x => x.tagName == "solution").innerXML;
@@ -25,11 +39,11 @@ results.childNodes.forEach(result => {
     });
 
     type(problem + solution + "\n" + targetText);
-})*/
+})
 
 
 var bigPRThreshold = 600;
 if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
   warn(':exclamation: Big PR');
   markdown('> Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.');
-}
+}*/

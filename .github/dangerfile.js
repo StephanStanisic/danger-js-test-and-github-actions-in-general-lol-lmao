29ed5660c2d7423751d0266f58339c80ff9ccdcb
Stephan Stanisic <stephan@stanisic.nl>
2020-11-11 17:11:08 +0100

    a

--- .github/dangerfile.js
+++ .github/dangerfile.js
@@ -1,6 +1,8 @@
-//import { message, warn, markdown, danger } from 'danger'
+import { message, warn, markdown, danger } from 'danger'
 const fs = require('fs');
 var parseString = require('xml2js').parseString;
+
+let counter = 0;
 
 var xml = fs.readFileSync('.github/artifacts/result.xml');
 parseString(xml, function (err, result) {
@@ -18,32 +20,13 @@ parseString(xml, function (err, result) {
             }
         });
 
-        type(problem + solution + "\n" + targetText);
+        type(`problem<sup>${++counter}`);
+        markdown(`> <sup>${counter}</sup>${solution}\n>${targetText}`);
     });
 });
 
-/*parsedXML["gendarme-output"].results.forEach(result => {
-    console.log("result", result);
-    let problem = result.childNodes.find(x => x.tagName == "problem").innerXML;
-    let solution = result.childNodes.find(x => x.tagName == "solution").innerXML;
-    let target = result.childNodes.find(x => x.tagName == "target").childNodes.filter(x => x.type == "element");
-    let targetText = target.map(x => x.attributes.Location + ": " + x.innerXML ).join("\n");
-    let name = result.attributes.Name;
-
-    console.log(problem, solution, targetText, name);
-
-    let type = message;
-    target.forEach(problem => {
-        if(problem.attributes.Severity == "High")
-            type = warn;
-    });
-
-    type(problem + solution + "\n" + targetText);
-})
-
-
 var bigPRThreshold = 600;
 if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
-  warn(':exclamation: Big PR');
-  markdown('> Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.');
-}*/
+  warn(`Big Pull Request<sup>${++counter}</sup>`);
+  markdown(`> <sup>${counter}</sup>Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.`);
+}

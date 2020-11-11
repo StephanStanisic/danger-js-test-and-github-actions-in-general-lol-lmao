08429c35b21fc88abe5a8d94bb91a214744f6228
Stephan Stanisic <stephan@stanisic.nl>
2020-11-11 15:38:56 +0100

    a

--- .github/dangerfile.js
+++ .github/dangerfile.js
@@ -22,3 +22,8 @@ results.childNodes.forEach(result => {
     type(problem + solution + "\n" + targetText);
 })
 
+var bigPRThreshold = 600;
+if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
+  warn(':exclamation: Big PR (' + ++errorCount + ')');
+  markdown('> (' + errorCount + ') : Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.');
+}
